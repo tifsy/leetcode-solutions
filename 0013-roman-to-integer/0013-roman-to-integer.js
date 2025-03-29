49adeffb141bb160f8ab8a95ea942d016df95f78
Seungyeon Han (Tiffany) <125551867+tifsy@users.noreply.github.com>
2025-03-30 01:18:21 +0900

    Time: 3 ms (94.04%), Space: 60.7 MB (40.22%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.js
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -2,6 +2,9 @@
  * @param {string} s
  * @return {number}
  */
+
+//hashtable, math, string 
+
 var romanToInt = function (s) {
     const roman = {
         'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
