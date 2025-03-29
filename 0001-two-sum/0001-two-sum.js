8dd442efccf2302bec0785f21d61f2676fced967
Seungyeon Han (Tiffany) <125551867+tifsy@users.noreply.github.com>
2025-03-29 19:14:19 +0900

    Time: 33 ms (38.81%), Space: 54.7 MB (35.87%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,7 +3,8 @@
  * @param {number} target
  * @return {number[]}
  */
- // index of 
+ // array 
+
 var twoSum = function(nums, target) {
     for(let i = 0; i < nums.length; i++){
         for(let j = i + 1; j < nums.length; j++){
