42dfe3b2eb0200baf8c45c1ede38a3750cb726d7
Seungyeon Han (Tiffany) <125551867+tifsy@users.noreply.github.com>
2025-03-31 01:29:26 +0900

    Time: 0 ms (100%), Space: 53.2 MB (51.68%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+
+//array, two pointers
+
+var removeElement = function (nums, val) {
+
+    let count = 0
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[count] = nums[i]
+            count++
+        }
+    }
+    return count
+
+};
