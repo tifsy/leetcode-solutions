a6c69fbd145d92609a9cf3c10f8213ad1e3eecbf
Seungyeon Han (Tiffany) <125551867+tifsy@users.noreply.github.com>
2025-04-12 04:08:54 +0900

    Time: 0 ms (100%), Space: 53.3 MB (69.02%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.js
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.js
@@ -2,12 +2,19 @@
  * @param {number[]} nums
  * @return {number}
  */
+//hash map 
+
 var numIdenticalPairs = function(nums) {
+    const map = {}
     let count = 0
-    for(let i in nums){
-        for(let j in nums){
-            if(nums[i] === nums[j] && i < j) count++
+
+    for(let a of nums){
+        if(map[a]){
+            count += map[a]
+            map[a]++
         }
+        else map[a] = 1
     }
-    return count 
+    return count
+
 };
