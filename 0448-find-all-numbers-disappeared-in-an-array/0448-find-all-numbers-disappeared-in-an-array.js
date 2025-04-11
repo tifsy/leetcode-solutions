550028e854ec9a0db6deda65d0c0b2fe70a4d995
Seungyeon Han (Tiffany) <125551867+tifsy@users.noreply.github.com>
2025-04-12 03:45:44 +0900

    Time: 18 ms (58.89%), Space: 77 MB (20.93%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.js
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.js
@@ -4,10 +4,11 @@
  */
  //array 
 var findDisappearedNumbers = function(nums) {
-
+    const set = new Set(nums)
+    
     let arr = []
-    for(let i = 1; i <= nums.length; i++){
-        arr.push(i)
+    for(let i = 1; i < nums.length + 1; i++){
+        if(!set.has(i)) arr.push(i)
     }
-    return arr.filter(a => !nums.includes(a))
+    return arr
 };
