903ad32ed71851763c9d77f371250a2f562d10f4
Seungyeon Han (Tiffany) <125551867+tifsy@users.noreply.github.com>
2025-04-15 11:03:00 +0900

    Time: 0 ms (100%), Space: 54.6 MB (47.97%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -3,12 +3,6 @@
  * @return {number[]}
  */
 var runningSum = function(nums) {
-    let ans = []
     let sum = 0
-
-    for(let i = 0; i < nums.length; i++){
-        sum += nums[i]
-        ans.push(sum)
-    }
-    return ans 
+    return nums.map(a => sum += a)
 };
