74f391da1ba1c92dc0dd569023e143e2e97a016b
Seungyeon Han (Tiffany) <125551867+tifsy@users.noreply.github.com>
2025-04-16 05:32:47 +0900

    Time: 0 ms (100%), Space: 55.8 MB (55.47%) - LeetHub

---
+++ 3162-find-the-number-of-good-pairs-i/3162-find-the-number-of-good-pairs-i.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @param {number} k
+ * @return {number}
+ */
+var numberOfPairs = function(nums1, nums2, k) {
+    let count = 0
+
+    for(let i = 0; i < nums1.length; i++){
+        for(let j = 0; j < nums2.length; j++){
+            nums1[i] % (nums2[j] * k) === 0 && count++
+        }
+    }
+    return count 
+};
