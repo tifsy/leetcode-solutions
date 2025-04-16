3aeccc509803d1a3f4a9119ab34b41670e4964d5
Seungyeon Han (Tiffany) <125551867+tifsy@users.noreply.github.com>
2025-04-16 22:03:43 +0900

    Time: 2 ms (88.32%), Space: 58.1 MB (23.35%) - LeetHub

--- 2974-minimum-number-game/2974-minimum-number-game.js
+++ 2974-minimum-number-game/2974-minimum-number-game.js
@@ -6,10 +6,9 @@ var numberGame = function(nums) {
     nums.sort((a, b) => a - b)
 
     let ans = []
-    
+
     for(let i = 0; i < nums.length / 2; i++){
-        ans.push(nums[2 * i + 1])
-        ans.push(nums[2 * i])
+        ans.push(nums[2 * i + 1], nums[2 * i])
     }
     return ans 
 };
