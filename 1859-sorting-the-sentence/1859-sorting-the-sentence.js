1b40ef229f57dfae5f93c4f00dcb38012e89560a
Seungyeon Han (Tiffany) <125551867+tifsy@users.noreply.github.com>
2025-04-16 22:37:43 +0900

    Time: 43 ms (43.84%), Space: 53.2 MB (71.13%) - LeetHub

---
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var sortSentence = function(s) {
+    const arr = s.split(' ')
+
+    let ans = []
+
+    for(let a of arr){
+        const index = [...a].pop()
+        const str = a.slice(0, a.length - 1)
+
+        ans[index - 1] = str
+    }
+    return ans.join(' ')
+};
