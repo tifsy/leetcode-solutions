6155f0e7678dd8417929d9f85dc9a642332beee7
Seungyeon Han (Tiffany) <125551867+tifsy@users.noreply.github.com>
2025-04-24 20:38:58 +0900

    Time: 18 ms (72.79%), Space: 54.1 MB (92.65%) - LeetHub

--- 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.js
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.js
@@ -7,6 +7,7 @@ var pickGifts = function(gifts, k) {
     for(let i = 0; i < k; i++){
         let max = Math.max(...gifts)
         let maxIndex = gifts.indexOf(max)
+        
         gifts[maxIndex] = Math.floor(Math.sqrt(max))
     }
     return gifts.reduce((acc, cur) => acc + cur, 0)
