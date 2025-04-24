06d06b95cf86aeb5e97227202980bb799f3b0111
Seungyeon Han (Tiffany) <125551867+tifsy@users.noreply.github.com>
2025-04-24 11:36:00 +0900

    Time: 0 ms (100%), Space: 55.9 MB (46.23%) - LeetHub

---
+++ 3151-special-array-i/3151-special-array-i.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isArraySpecial = function(nums) {
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] % 2 === nums[i + 1] % 2) return false 
+    }
+    return true 
+};
