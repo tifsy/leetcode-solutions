3d2107a7189a253d5c6a5cae70e15e2f53cb5f5b
Seungyeon Han (Tiffany) <125551867+tifsy@users.noreply.github.com>
2025-05-09 23:13:20 +0900

    Time: 1 ms (64.56%), Space: 55.5 MB (40%) - LeetHub

---
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string[]} details
+ * @return {number}
+ */
+var countSeniors = function(details) {
+    let count = 0
+    
+    for(let a of details){
+        a.slice(11, 13) > 60 && count++
+    }
+    return count 
+};
