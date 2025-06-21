67340c78b071a7f89a8c6aa1d7a590ecf07834c1
Seungyeon Han (Tiffany) <125551867+tifsy@users.noreply.github.com>
2025-06-22 02:20:09 +0900

    Time: 40 ms (67.93%), Space: 53.6 MB (45.15%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.js
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.js
@@ -3,11 +3,5 @@
  * @return {number}
  */
 var numberOfMatches = function(n) {
-    let sum = 0
-
-    while(n !== 1){
-        sum += Math.floor(n / 2)
-        n = Math.ceil(n / 2)
-    }
-    return sum 
+    return n - 1
 };
